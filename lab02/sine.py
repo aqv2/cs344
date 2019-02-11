f5ddfc7176ae91b8b4c1ba2ae0162dc4e739f868
Alastair Van Maren <aqv2@maroon22.cs.calvin.edu>
2019-02-11 08:35:05 -0500

    Finishing up lab 02 (added random restarts)

--- lab02/sine.py
+++ lab02/sine.py
@@ -9,6 +9,7 @@ from tools.aima.search import Problem, hill_climbing, simulated_annealing, \
 from random import randrange
 import math
 import time
+import numpy as np
 
 
 class AbsVariant(Problem):
@@ -36,29 +37,49 @@ if __name__ == '__main__':
     # Formulate a problem with a 2D hill function and a single maximum value.
     maximum = 30
     initial = randrange(0, maximum)
-    initial = randrange(0, maximum)
     p = AbsVariant(initial, maximum, delta=1.0)
     print('Initial                      x: ' + str(p.initial)
           + '\t\tvalue: ' + str(p.value(initial))
           )
 
     # Solve the problem using hill-climbing.
-    start = time.time()
-    hill_solution = hill_climbing(p)
-    print('Hill-climbing solution       x: ' + str(hill_solution)
-          + '\tvalue: ' + str(p.value(hill_solution))
-          )
-    end = time.time()
-    print('\t' + str(end - start))
+    hill_climbing_results = []
+    hill_climbing_speeds = []
+    for i in range(1000):
+        initial = randrange(0, maximum)
+        p = AbsVariant(initial, maximum, delta=1.0)
+        start = time.time()
+        hill_solution = hill_climbing(p)
+        end = time.time()
+        print('Hill-climbing solution       x: ' + str(hill_solution)
+              + '\tvalue: ' + str(p.value(hill_solution))
+              )
+        hill_climbing_speeds.append(end - start)
+        hill_climbing_results.append(p.value(hill_solution))
+        print('\t' + str(end - start))
 
     # Solve the problem using simulated annealing.
-    start2 = time.time()
-    annealing_solution = simulated_annealing(
-        p,
-        exp_schedule(k=20, lam=0.005, limit=1000)
-    )
-    print('Simulated annealing solution x: ' + str(annealing_solution)
-          + '\tvalue: ' + str(p.value(annealing_solution))
-          )
-    end2 = time.time()
-    print('\t' + str(end2 - start2))
+    annealing_results = []
+    annealing_speeds = []
+    for i in range(1000):
+        initial = randrange(0, maximum)
+        p = AbsVariant(initial, maximum, delta=1.0)
+        start2 = time.time()
+        annealing_solution = simulated_annealing(
+            p,
+            exp_schedule(k=20, lam=0.005, limit=1000)
+        )
+        end2 = time.time()
+        print('Simulated annealing solution x: ' + str(annealing_solution)
+              + '\tvalue: ' + str(p.value(annealing_solution))
+              )
+        annealing_speeds.append(end2 - start2)
+        annealing_results.append(p.value(annealing_solution))
+        print('\t' + str(end2 - start2))
+
+    print('Hill-climbing max: ' + str(max(hill_climbing_results)))
+    print('Hill-climbing avg: ' + str(sum(hill_climbing_results) / len(hill_climbing_results)))
+    print('Hill-climbing avg speed: ' + (str(sum(hill_climbing_speeds) / len(hill_climbing_speeds))))
+    print('Simulated annealing max: ' + str(max(annealing_results)))
+    print('Simulated annealing avg: ' + str(sum(annealing_results) / len(annealing_results)))
+    print('Simulated annealing avg speed: ' + (str(sum(annealing_speeds) / len(hill_climbing_speeds))))
