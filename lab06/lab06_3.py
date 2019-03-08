f85465d6e40cffdf75d16257c9811502ce7c83f0
aqv2 <aqv2@students.calvin.edu>
2019-03-08 09:51:27 -0500

    Adding lab 06

---
+++ lab06/lab06_3.py
@@ -0,0 +1,22 @@
+import numpy as np
+from keras.datasets import boston_housing
+
+(train_images, train_labels),  (test_images, test_labels) = boston_housing.load_data()
+def print_structures():
+    print(
+        'training images \
+            \n\tcount: {len(train_images)} \
+            \n\tdimensions: {train_images.ndim} \
+            \n\tshape: {train_images.shape} \
+            \n\tdata type: {train_images.dtype}\n\n',
+        'testing images \
+            \n\tcount: {len(test_labels)} \
+            \n\tdimensions: {train_labels.ndim} \
+            \n\tshape: {test_labels.shape} \
+            \n\tdata type: {test_labels.dtype} \
+            \n\tvalues: {test_labels}\n',
+    )
+
+
+print_structures()
+
