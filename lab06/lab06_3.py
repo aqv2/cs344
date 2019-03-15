879ad3ee5f69922dea8d90d8b06e9f46a2d1c1c3
aqv2 <aqv2@students.calvin.edu>
2019-03-15 10:14:42 -0400

    Fixed the keras errors and added lab07

--- lab06/lab06_3.py
+++ lab06/lab06_3.py
@@ -4,17 +4,17 @@ from keras.datasets import boston_housing
 (train_images, train_labels),  (test_images, test_labels) = boston_housing.load_data()
 def print_structures():
     print(
-        'training images \
-            \n\tcount: {len(train_images)} \
-            \n\tdimensions: {train_images.ndim} \
-            \n\tshape: {train_images.shape} \
-            \n\tdata type: {train_images.dtype}\n\n',
-        'testing images \
-            \n\tcount: {len(test_labels)} \
-            \n\tdimensions: {train_labels.ndim} \
-            \n\tshape: {test_labels.shape} \
-            \n\tdata type: {test_labels.dtype} \
-            \n\tvalues: {test_labels}\n',
+        'training images\n\tcount: ' + str({len(train_images)}) +
+            '\n\tdimensions: ' + str({train_images.ndim}) +
+            '\n\tshape: ' + str({train_images.shape}) +
+            '\n\tdata type: ' + str({train_images.dtype}) +'\n\n',
+        'testing images' +
+            '\n\tcount: ' + str({len(test_labels)}) +
+            '\n\tdimensions: ' + str({train_labels.ndim}) +
+            '\n\tshape: ' + str({test_labels.shape}) +
+            '\n\tdata type: ' + str({test_labels.dtype})
+            # + '\n\tvalues: ' + str({test_labels})
+            # for some reason the above line gives an "unhashable type" error. The rest of it works, though.
     )
 
 
